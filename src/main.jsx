8774139a5b4de2b123b12ab46235bb0fd3d4ef89
Yuval207 <yuvalsharma49@gmail.com>
2024-03-16 18:34:10 +0530

    Co-authored-by: Harigovind T P <HariTP@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signin from "./components/Signin.jsx";
 import Signup from "./components/Signup.jsx";
 import Problems from "./components/Problems.jsx";
-import Editor from "./components/EditorPage.jsx";
+import EditorPage from "./components/Editor/EditorPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/editor" element={<Editor />} />
+        <Route path="/editor" element={<EditorPage />} />
         <Route path="/problems" element={<Problems />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
