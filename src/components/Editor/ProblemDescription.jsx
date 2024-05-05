d996d2d89be9f782af7faf186b7437fd19e00146
Harigovind T P <harigvnd2003@gmail.com>
2024-05-05 23:28:32 +0530

    Added Testcase and test result section.

--- src/components/Editor/ProblemDescription.jsx
+++ src/components/Editor/ProblemDescription.jsx
@@ -7,6 +7,9 @@ import { useState, useEffect } from "react";
 
 const ProblemDescription = (props) => {
   const [data, setData] = useState(null);
+  const [testcase, setTestcase] = useState([]);
+  const [istestcase, setIstestcase] = useState(true);
+
   useEffect(() => {
     getProblemDescription(props.id).then((problems) => setData(problems));
 
@@ -71,14 +74,50 @@ const ProblemDescription = (props) => {
           </p>
         </div>
 
-        {/* <div style={{ background: "#333", color: "#fff" }} className="p-4">
-          Testcases
-          <p>
-            <li>case 1</li>
-            <li>case 1</li>
-            <li>case 1</li>
-          </p>
-        </div> */}
+        <div
+          style={{ background: "#333", color: "#fff" }}
+          className="overflow-auto"
+        >
+          <div className="flex gap-7 bg-gray-700 pl-4 h-12">
+            <button
+              className={`rounded-xl ${istestcase ? "bg-gray-800" : ""} p-2`}
+              onClick={() => {
+                setIstestcase(true);
+              }}
+            >
+              Testcases
+            </button>
+            <button
+              className={`rounded-xl p-2 ${istestcase ? "" : "bg-gray-800"}`}
+              onClick={() => {
+                setIstestcase(false);
+              }}
+            >
+              Test Result
+            </button>
+          </div>
+          <div className="m-4">
+            {istestcase ? (
+              <div className="flex flex-col">
+                <div className="flex gap-7 ">
+                  <button>Case 1</button>
+                  <button>Case 2</button>
+                  <button>Case 3</button>
+                </div>
+                <p className="mt-5">nums = </p>
+                <div className="mt-2 bg-gray-800 w-full rounded-2xl h-12 p-1 flex pl-4 items-center">
+                  [1,2,3,4,5,6]
+                </div>
+                <p className="mt-5">target = </p>
+                <div className="mt-2 bg-gray-800 w-full rounded-2xl h-12 p-1 flex pl-4 items-center">
+                  5
+                </div>
+              </div>
+            ) : (
+              <p>Test Result</p>
+            )}
+          </div>
+        </div>
       </Split>
     </>
   );
